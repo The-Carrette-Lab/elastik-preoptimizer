c607b7f130221d7d549633b099e54dfd15566d82
Boris Ryabov <stonks33@gmail.com>
2024-02-11 17:48:29 -0800

    [REFACTOR] refactor to exclude multiple inits

--- preprocessor.py
+++ preprocessor.py
@@ -1,26 +1,19 @@
 import numpy as np
 class Preprocessor:
-    raw: np.array
-    avg_value: int
-
-    def __init__(self, raw: np.array):
-        self.raw = raw
-        self.avg_value = np.average(raw)
-
-    def preprocess(self, lower_value: int) -> np.array:
-        return_array = np.copy(self.raw)
+    def preprocess(self, raw: np.array, lower_value: int =0) -> np.array:
+        avg_value = np.average(raw)
 
         # background exclusion
-        for (x, y, z), value in np.ndenumerate(return_array):
-            value = return_array[x][y][z]
+        for (x, y, z), value in np.ndenumerate(raw):
+            value = raw[x][y][z]
 
-            if value < self.avg_value or value < lower_value:
-                return_array[x][y][z] = 0
+            if value < avg_value or value < lower_value:
+                raw[x][y][z] = 0
 
         # normalization
-        return_array = (return_array - np.min(return_array)) / \
-            (np.max(return_array) - np.min(return_array))
+        raw = (raw - np.min(raw)) / \
+            (np.max(raw) - np.min(raw))
 
             
-        return return_array
+        return raw
     
